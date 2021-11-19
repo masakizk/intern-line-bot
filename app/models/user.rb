047ed3b322699d1f652b16de519baca674236646
ishizakamasaki <robomasaki2@gmail.com>
2021-11-19 16:39:18 +0900

    feat(model): create user from LINE
    LINEユーザーからUserを作成するメソッドを作成した。
    アプリケーションがLINEに依存しないようにするため、Userを介して様々な操作ができるようにする。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_many :wakeups
+
+  # LINEのuser idからユーザーを作成する
+  def self.create_from_line_user(user_id)
+    # LINEユーザーとして登録されていないときのみ、作成する
+    if User.exists?(line_user_id: user_id)
+      return false
+    end
+
+    User.create!(line_user_id: user_id)
+  end
 end
